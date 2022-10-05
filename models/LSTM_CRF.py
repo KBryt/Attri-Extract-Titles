ab20bd37797808d150d99ffd83591d0a5619f0e3
Bright Effah <99838742+KBryt@users.noreply.github.com>
2022-10-05 13:24:45 -0400

    Update LSTM_CRF.py

--- models/LSTM_CRF.py
+++ models/LSTM_CRF.py
@@ -26,7 +26,7 @@ class NERLSTM_CRF(BasicModule):
         self.hidden2tag = nn.Linear(self.hidden_dim, self.tagset_size)
         self.crf = CRF(self.tagset_size)
 
-    def forward(self, inputs, **kwardgs):
+    def forward(self, inputs):
         x, att, tags = inputs
         #CRF
         x = x.transpose(0,1)
